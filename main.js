11749455f1eebea9504c10c670e0214376404f51
Arjun G Krishna <arjungk@pm.me>
2022-06-05 18:45:13 +0530

    feat: :fire: implemented blink history in the UI

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getDatabase, ref, onChildAdded } from "firebase/database";
+import { getDatabase, ref, onValue } from "firebase/database";
 document.getElementById('notifier').onclick = function(){notifyMe()};
+const his = document.getElementById('history');
+var buffer= [] ;
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -23,36 +25,25 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 // Get a reference to the database service
 const database = getDatabase(app);
-const count = ref(database, 'patients/');
-onValue(count, (snapshot) => {
-  const data = snapshot.val();
+
+const countRef = ref(database, 'patients/');
+onValue(countRef, (snapshot) => {
+  const data = snapshot.val();  
+  buffer = data;
+  his.innerText = ' '
+  for(var key in buffer){
+    his.innerText += "Blinks: "+buffer[key]['BlinkCount']+" Time: "+buffer[key]['time']+'\n'
+  }
   var notification = new Notification("Alert! Please attend to your patient");
 });
-// onChildAdded(count, () => {
-//   alert("Alert! Your patient needs to be attended to.");
-// })
-// const recentPostsRef = query(ref(database, 'posts'), limitToLast(1));
-// console.log(recentPostsRef)
 function notifyMe() {
   // Let's check if the browser supports notifications
   if (!("Notification" in window)) {
     alert("This browser does not support desktop notification");
   }
-
-  // Let's check whether notification permissions have already been granted
-  // else if (Notification.permission === "granted") {
-  //   // If it's okay let's create a notification
-  //   var notification = new Notification("Hi there!");
-  // }
-
-  // Otherwise, we need to ask the user for permission
+  // Ask the user for permission
   else if (Notification.permission !== "denied") {
-    Notification.requestPermission().then(function (permission) {
-      // If the user accepts, let's create a notification
-      if (permission === "granted") {
-        var notification = new Notification("Alert! Please attend to your patient");
-      }
-    });
+    Notification.requestPermission()
   }
   // At last, if the user has denied notifications, and you
   // want to be respectful there is no need to bother them anymore.
